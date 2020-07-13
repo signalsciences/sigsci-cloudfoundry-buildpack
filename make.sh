a4a5e7e05e325ef8501fd5d90bd0530f156cac96
bbucher <bbucher@signalsciences.com>
2020-07-13 11:13:05 -0500

    simplify workflow

--- make.sh
+++ make.sh
@@ -1,17 +1,2 @@
 #!/bin/sh
 
-# This should only be run only via jenkins
-
-VERSION="$(cat VERSION)"
-PKG_NAME="sigsci-cloudfoundry-buildpack"
-
-KEYBASE="${PKG_NAME}/${GITHUB_RUN_NUMBER}"
-TARBALL=sigsci-cloudfoundry-buildpack_${VERSION}.tgz
-
-# Use a different name for the readme to avoid overwriting any README.md in the
-# client's buildpack, include the version in the README
-echo "Version: ${VERSION}" > README-SIGSCI.md
-cat README.md >> README-SIGSCI.md
-
-# create tarball
-tar -zcvf $TARBALL README-SIGSCI.md bin/ lib/
